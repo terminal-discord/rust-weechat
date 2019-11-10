191014c8f5a1fae4f99f38cd20461ea30a493c83
Noskcaj <kcajdev@gmail.com>
2019-11-10 06:03:05 -0500

    weechat: Add buffer methods for updating the hotlist

--- weechat-rs/src/buffer.rs
+++ weechat-rs/src/buffer.rs
@@ -271,6 +271,26 @@ impl<'a> Default for NickArgs<'a> {
     }
 }
 
+#[derive(Debug, Clone, Copy, PartialEq)]
+pub enum HotlistPriority {
+    Low,
+    Message,
+    Private,
+    Highlight,
+}
+
+impl HotlistPriority {
+    fn to_c_rep(&self) -> &'static str {
+        use HotlistPriority::*;
+        match self {
+            Low => "0",
+            Message => "1",
+            Private => "2",
+            Highlight => "3",
+        }
+    }
+}
+
 impl Buffer {
     /// Create a high level Buffer object from a C plugin pointer and the
     /// buffer pointer.
@@ -571,9 +591,23 @@ impl Buffer {
     }
 
     /// Remove buffer from the hotlist.
-    // TODO: Add support for other capabilities
     pub fn clear_hotlist(&self) {
         self.set("hotlist", "-1");
+    }
+
+    /// Enable hotlist
+    pub fn enable_hotlist(&self) {
+        self.set("hotlist", "+");
+    }
+
+    /// Disable hotlist
+    pub fn disable_hotlist(&self) {
+        self.set("hotlist", "-");
+    }
+
+    /// Add buffer to the hotlist.
+    pub fn set_hotlist(&self, priority: HotlistPriority) {
+        self.set("hotlist", priority.to_c_rep());
     }
 
     /// Clear buffer contents
